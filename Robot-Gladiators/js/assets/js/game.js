1080411c143fd95cd07cfe1dd77f0025c6fe6a4f
Xavier <X64515@yahoo.com>
2020-10-07 19:24:42 -0500

    done w 3.1.8

--- Robot-Gladiators/js/assets/js/game.js
+++ Robot-Gladiators/js/assets/js/game.js
@@ -1,1 +1,47 @@
-window.alert("This is an alert! JavaScript is running!");
+let playerName = window.prompt("What is your robot's name?");
+console.log(playerName)
+let playerHealth = 100;
+let playerAttack =10;
+
+let enemyName = "Roborto";
+let enemyHealth = 50;
+let enemyAttack =12;
+
+function fight(){
+
+window.alert("Welcome to robot Gladiator");
+
+playerHealth = playerHealth-enemyAttack;
+console.log(`${enemyName} attacked ${playerName}. ${playerName} now has ${playerHealth} health remaining`);
+
+console.log(
+    playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
+  );
+  if (playerHealth <= 0) {
+    window.alert(playerName + " has died!");
+  } 
+  else {
+    window.alert(playerName + " still has " + playerHealth + " health left.");
+  }
+  
+  // check enemy's health
+  if (enemyHealth <= 0) {
+    window.alert(enemyName + " has died!");
+  } 
+  else {
+    window.alert(enemyName + " still has " + enemyHealth + " health left.");
+  }
+
+  
+}
+fight();
+
+enemyHealth = enemyHealth - playerAttack;
+console.log(enemyHealth);
+
+if (playerHealth > 0){
+    console.log("your player is still alive");
+}
+else{
+    console.log("this will run instead");
+}
